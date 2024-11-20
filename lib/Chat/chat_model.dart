47c334957d9954650470ff0d5ab7aaa348aab07a
Enzo Soares <enzorodrigues.2003@hotmail.com>
2024-11-20 16:16:40 -0300

    Adiciona visualizacao de imagem selecionada no chat

--- lib/Chat/chat_model.dart
+++ lib/Chat/chat_model.dart
@@ -2,11 +2,13 @@ class MessageModel {
   //final int id;
   final String message;
   final MessageFrom messageFrom;
+  final String? imagePath;
 
   MessageModel({
     //required this.id,
     required this.message,
     required this.messageFrom,
+    this.imagePath
   });
 
   Map<String, Object?> toMap() {
@@ -14,12 +16,13 @@ class MessageModel {
       //'id': id,
       'message': message,
       'who': messageFrom,
+      'image': imagePath
     };
   }
 
   @override
   String toString() {
-    return 'Message{message: $message, age: $messageFrom}';
+    return 'Message{message: $message, from: $messageFrom, image: $imagePath}';
   }
 }
 
