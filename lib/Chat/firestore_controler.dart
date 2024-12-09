8d83dd6d45b472a533cb03f2cbec4bb3b6c16f1a
Enzo Soares <enzorodrigues.2003@hotmail.com>
2024-12-09 16:31:51 -0300

    Ajustando recuperacao de mensagens da nuvem

--- lib/Chat/firestore_controler.dart
+++ lib/Chat/firestore_controler.dart
@@ -1,23 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:jarvis_assistant/Chat/chat_model.dart';
+import 'package:jarvis_assistant/Utils/configs.dart';
+
 
 class FirestoreController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Salva uma mensagem no Firestore
   Future<void> saveMessage(MessageModel message) async {
-    await _firestore.collection('messages').add(message.toMap());
+    await _firestore.collection('usuarios/${loggedUser.userID}/messages').add(message.toMap());
   }
 
-  // Obtém todas as mensagens do Firestore
-  Stream<List<MessageModel>> getMessages() {
-    return _firestore
-        .collection('messages')
-        .orderBy('timestamp', descending: false)
-        .snapshots()
-        .map((snapshot) =>
-            snapshot.docs.map((doc) => MessageModel.fromMap(doc.data())).toList());
-  }
+  Future<List<MessageModel>> getMessages() async {
+    List<MessageModel> messages = [];
+    final snapshot = await _firestore.collection('usuarios/${loggedUser.userID}/messages').get();
+    snapshot.docs.forEach((doc) {
+      messages.add(MessageModel.fromMap(doc.data()));
+    });
 
-  
+    return messages;
+  }
 }
