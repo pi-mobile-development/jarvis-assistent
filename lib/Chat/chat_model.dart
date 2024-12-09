7abdfeed5a6a55ee6a1494a33ca52f6a7c4e1a92
Rikingg <soaresitalohenrique@gmail.com>
2024-12-09 16:06:01 -0300

    Inclusão do timestamp para o message model

--- lib/Chat/chat_model.dart
+++ lib/Chat/chat_model.dart
@@ -1,14 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class MessageModel {
-  //final int id;
   final String message;
   final MessageFrom messageFrom;
   final String? imagePath;
+  final Timestamp? timestamp; // Corrigido: FieldValue para Timestamp
 
   MessageModel({
-    //required this.id,
     required this.message,
     required this.messageFrom,
-    this.imagePath
+    this.timestamp,
+    this.imagePath,
   });
 
   // Converte para Map para salvar no Firestore
@@ -17,6 +19,7 @@ class MessageModel {
       'message': message,
       'messageFrom': messageFrom.name, // Salva como string
       'imagePath': imagePath,
+      'timestamp': FieldValue.serverTimestamp(), // Incluído para garantir a consistência
     };
   }
 
@@ -24,14 +27,18 @@ class MessageModel {
   factory MessageModel.fromMap(Map<String, dynamic> map) {
     return MessageModel(
       message: map['message'] ?? '',
-      messageFrom: MessageFrom.values.firstWhere((e) => e.name == map['messageFrom']),
+      messageFrom: MessageFrom.values.firstWhere(
+        (e) => e.name == map['messageFrom'],
+        orElse: () => MessageFrom.USER, // Valor padrão em caso de erro
+      ),
+      timestamp: map['timestamp'] ?? Timestamp.now(), // Corrigido
       imagePath: map['imagePath'],
     );
   }
 
   @override
   String toString() {
-    return 'Message{message: $message, from: $messageFrom, image: $imagePath}';
+    return 'Message{message: $message, from: $messageFrom, image: $imagePath, timestamp: $timestamp}';
   }
 }
 
